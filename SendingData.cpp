b846855b112ffb3a93336b9de2d3544b407fa9d0
KasparByrne <103092736+KasparByrne@users.noreply.github.com>
2022-04-11 18:03:59 +1000

    Create SendingData.cpp

---
+++ SendingData.cpp
@@ -0,0 +1,15 @@
+// This #include statement was automatically added by the Particle IDE.
+#include <Adafruit_DHT.h>
+
+DHT dht(D4, DHT22); //(pin, model)
+
+void setup() {
+    Serial.begin(9600); //set the analog rate?
+    dht.begin(); //start reading data from the DHT22
+}
+
+void loop() {
+    delay(30000);
+    String temp = String(dht.getTempCelcius()); //Get temperature from DHT22
+    Particle.publish("temp", temp, PRIVATE); //Publish temp data to ThingSpeak
+}
